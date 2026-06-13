e61814a3be09004f23ddb9fd4e2df1bb2288d18a
gskaboagye <gskaboagye@gmail.com>
2026-06-13 20:42:53 +0000

    commit

--- src/swagger/swagger.js
+++ src/swagger/swagger.js
@@ -1,21 +1,33 @@
 const swaggerJsdoc = require("swagger-jsdoc");
 
 const options = {
-  definition: {
-    openapi: "3.0.0",
-    info: {
-      title: "Portfolio Builder API",
-      version: "1.0.0",
-      description:
-        "API for Portfolio Builder Project",
-    },
-    servers: [
-      {
-        url: "https://portfolio-builder-api-v1jr.onrender.com",
-      },
-    ],
-  },
-  apis: ["./src/routes/*.js"],
+definition: {
+openapi: "3.0.0",
+info: {
+title: "Portfolio Builder API",
+version: "1.0.0",
+description: "API for Portfolio Builder Project",
+},
+servers: [
+{
+url: "https://portfolio-builder-api-v1jr.onrender.com",
+description: "Render Production Server",
+},
+{
+url: "http://localhost:3000",
+description: "Local Development Server",
+},
+],
+},
+apis: [
+"./src/routes/user.js",
+"./src/routes/theme.js",
+"./src/routes/project.js",
+"./src/routes/skill.js",
+"./src/routes/auth.js",
+],
 };
 
-module.exports = swaggerJsdoc(options);
+const swaggerSpec = swaggerJsdoc(options);
+
+module.exports = swaggerSpec;
